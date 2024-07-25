0f6bda738d8588b04e83dc7a53fd0e80b9275f3e
Rupesh <rupesh@Rupeshs-MacBook-Air.local>
2024-07-25 15:58:55 +0530

    api

--- src/App.jsx
+++ src/App.jsx
@@ -144,35 +144,134 @@
 //   </>)
 // }
 
+
+// export default App
+// import React, { useEffect, useState } from 'react'
+// import './App.css'
+// const App = () => {
+//   let [data,SetData]=useState([])
+
+// useEffect(()=>{
+//   fetch('https://dummyjson.com/recipes').then((res)=>{
+//     return res.json()
+
+//   }).then((data)=>{
+//     console.log(data.recipes);
+//     SetData(data.recipes)
+//     console.log(data,"datat");
+
+//   })
+
+// },[])   
+
+
+// function fun1(id){
+//   // console.log(a,"hehehe");
+//  let newA= data.filter((a,b,c)=>{
+//     return a.id!=id
+
+//   })
+//   SetData(newA)
+
+// }
+
+//   return (
+//     <div>
+//       {
+//         data.map((a,index)=>{
+//           return(<>
+//           <div id='parent'>  
+//           <p>{a.id}</p>
+//           <p>{a.name}</p>
+//           <h5>{a.cuisine}</h5>
+//           <button onClick={()=>fun1(a.id)}>delet</button>
+//           {/* <img   height='150' src={a.image}/> */}
+//           </div>
+//           </>)
+
+//         })
+//       }
+//     </div>
+//   )
+// }
+
 // export default App
 
 
 
-
-import React from 'react'
-import SignUp from './SignUp'
-import { Route ,Routes} from 'react-router-dom'
-import Login from './Login'
-
-
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+  let arr=[1,2,3,4,5,6,67,7]
+
+  let sum=arr.reduce((a,b)=>{
+    return a+b
+  })
+  console.log(sum,"heheh");
+
+
+
+let [data,SetData]=useState([])
+
+ useEffect(()=>{
+  fetch('https://dummyjson.com/recipes').then((D)=>{
+    return D.json()
+
+  }).then((d)=>{
+    SetData(d.recipes)
+    console.log('heyeye');
+
+  })
+
+
+ },[])
+
+function fun1(yewobuttonhai){
+  // console.log(id,'heheh');
+ let newD=  data.filter((value,b)=>{
+    return value.id!=yewobuttonhai
+
+  })
+  console.log(newD,"heheh");
+  SetData(newD)
+
+}
+
   return (
     <div>
 
+{
+  data.map((a)=>{
+    return(<>
+    <p>{a.id}</p>
+  
+    <p>{a.name}</p>
+    <p>{a.cuisine}</p>
+    <button onClick={()=>fun1(a.id)}> delet</button>
+   <img   height='100' src={a.image}/>
+    </>)
+})
+}
 
-  <Routes>
-  <Route   path='/'   element={<SignUp/>}/>
-  <Route   path='/login'   element={<Login/>}/>
-
-  </Routes>
-
-      
-       </div>
+    </div>
   )
 }
 
 export default App
 
+
+
+
+
+
+
+
+
+
+// function sum(a){
+
+// }
+// sum(5)
 
 
--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,55 +1,45 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-
 const SignUp = () => {
-  let [input, setInput] = useState({
-    name: '',
-    mail: '',
-    password: ''
+  let [input,SetInput]=useState({
+    name:"",
+    email:"",
+    password:""
   })
-  let [data, setData] = useState([])
 
-  function fun1(e) {
-    let { name, value } = e.target
-    setInput({ ...input, [name]: value })
-  }
 
-  function save() {
-    const newData = [...data, input]
-    setData(newData)
-    localStorage.setItem('userData', JSON.stringify(newData))
-  }
+  // localStorage.setItem('key','coding sidugcs78fg87gr')
+
+ function fun1(e){ 
+  // console.log(e.target,"hhehehe");
+  let {name,value}=   e.target
+  // console.log(e.target.value);
+  SetInput({...input,[name]:value})
+  console.log(input);
+
+
+ }
+
+ function done(){
+  localStorage.setItem('user',JSON.stringify(input))
+
+   let data=  localStorage.getItem('user')
+   console.log(  JSON.parse(data));
+
+ }
 
   return (
     <div>
-      <h2>Signup</h2>
-      <input
-        type='text'
-        onChange={fun1}
-        name='name'
-        value={input.name}
-        placeholder='Enter your name'
-      />
+      <input  type='text' name='name' value={input.name} onChange={fun1} placeholder='Enter your name'/>
       <br/>
-      <input
-        type='email'
-        onChange={fun1}
-        name='mail'
-        value={input.mail}
-        placeholder='Enter your email'
-      />
       <br/>
-      <input
-        type='password'
-        onChange={fun1}
-        name='password'
-        value={input.password}
-        placeholder='Enter your password'
-      />
+
+      <input  type='email' name='email' value={input.email}  onChange={fun1} placeholder='Enter your email'/>
       <br/>
-      <button onClick={save}>Save</button>
       <br/>
-      <Link to='/login'>Login</Link>
+
+      <input   type='password'  name='password' value={input.password} onChange={fun1} placeholder='Enter your password'/>
+<button onClick={done}>save </button>
+
     </div>
   )
 }
